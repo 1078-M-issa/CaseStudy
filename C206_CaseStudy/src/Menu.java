29d32ad28355b7e7870d3a25bc9b93ebb0e327f3
Paulo003 <21009874@myrp.edu.sg>
2022-08-05 14:38:32 +0800

    pushed updated calss

---
+++ C206_CaseStudy/src/Menu.java
@@ -0,0 +1,37 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Paulo Diaz
+ * Student ID: 21009874
+ * Class: W64N
+ * Date/Time created: Friday 05-08-2022 14:22
+ */
+
+/**
+ * @author paulo
+ *
+ */
+public class Menu extends Stall{
+	private String foodName;
+	private double price;
+	
+	public Menu(int stall_id, String foodName, double price) {
+		super(stall_id);
+		this.foodName = foodName;
+		this.price = price;
+	}
+	public String toString() {
+		
+		String foodItem = String.format("%-10s %-30f",  foodName, price);
+		return foodItem;
+	}
+	public String getfoodName() {
+		return foodName;
+	}
+	public double getPrice() {
+		return price;
+	}
+
+}
